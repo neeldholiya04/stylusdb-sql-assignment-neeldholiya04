711614d2fd4b3c6e049b5dd5c25ea23498518018
Neel Dholiya <neeldholiya04@gmail.com>
2024-04-01 16:57:55 +0530

    Assignment 1 to 4 completed

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const parseQuery = require('./queryParser');
+const readCSV = require('./csvReader');
+
+async function executeSELECTQuery(query) {
+    try {
+        const { fields, table } = parseQuery(query);
+        const data = await readCSV(`${table}.csv`);
+        
+        // Filter the fields based on the query
+        return data.map(row => {
+            const filteredRow = {};
+            fields.forEach(field => {
+                filteredRow[field] = row[field];
+            });
+            return filteredRow;
+        });
+    } catch (error) {
+        return { 
+            error: 'File not found or error reading CSV' 
+        };
+    }
+}
+
+module.exports = executeSELECTQuery;
